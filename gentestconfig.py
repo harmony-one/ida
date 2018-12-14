abba3c30a3ac8464fd30ed07ab2ced51aa7d03dd
Eugene Kim <ek@harmony.one>
2018-12-13 19:51:42 -0800

    Update the test helper script
    Prerequisites:
    
    - Equal number (e.g. 100) of Ubuntu instances running per region, with
      Name tag “IDA Test Instance” and the ida repo compiled/installed in
      ~ubuntu/go/src/github.com/harmony-one/ida
    - API keys in the [default] section of ~/.aws/credentials
    - SSH key in ~/.ssh/ida
    - Python 3.7
    
    usage: gentestconfig.py [-h] [--profile PROFILE] [--name NAME_TAG]
                            [--ssh-key FILE] [--ssh-user FILE] [--ida-dir FILE]
                            [--action ACTION] [--file FILE]
                            N REGION [REGION ...]
    
    positional arguments:
      N                  number of instances
      REGION             AWS regions (such as us-west-2)
    
    optional arguments:
      -h, --help         show this help message and exit
      --profile PROFILE  profile section in ~/.aws/credentials
      --name NAME_TAG    instance name tag to match (default: IDA Test Instance)
      --ssh-key FILE     SSH key file to use (default: /Users/ek/.ssh/ida)
      --ssh-user FILE    remote SSH username (default: ubuntu)
      --ida-dir FILE     harmony-one/ida repository path on instance (default:
                         go/src/github.com/harmony-one/ida)
      --action ACTION    action(s) to take, a comma-separated list of: gen, start,
                         stop, send, update
      --file FILE        the file to send (for --action=send)
    
    To generate/upload config onto the 100 instances running in five regions:
    
    python3.7 -m gentestconfig --action=gen 100 us-east-2 us-east-1 us-west-2 eu-west-1 ap-southeast-1
    
    To start the servers (receivers), use --action=start.
    
    To send a file, use --action=send --file=FILENAME (--action defaults to
    send, so you may just use --file).
    
    To stop the servers (receivers) and client (sender), use --action=stop.
    
    Multiple actions may be separated by commas, ex:
    
    python3.7 -m gentestconfig --action=stop,gen,start,send --file=omg.pdf 100 us-east-2 us-east-1 us-west-2 eu-west-1 ap-southeast-1

--- gentestconfig.py
+++ gentestconfig.py
@@ -34,6 +34,11 @@ def _main():
     parser.add_argument('--ida-dir', metavar='FILE',
                         help=f"""harmony-one/ida repository path on instance
                                  (default: {DEFAULT_IDA_DIR})""")
+    parser.add_argument('--action', metavar='ACTION', dest='actions',
+                        help=f"""action(s) to take, a comma-separated list of:
+                                 gen, start, stop, send, update""")
+    parser.add_argument('--file', metavar='FILE',
+                        help=f"""the file to send (for --action=send)""")
     parser.add_argument('num_instances', type=int, metavar='N',
                         help="""number of instances""")
     parser.add_argument('regions', nargs='+', metavar='REGION',
@@ -42,7 +47,8 @@ def _main():
                         profile='default',
                         ssh_key=DEFAULT_SSH_KEY,
                         ssh_user='ubuntu',
-                        ida_dir=DEFAULT_IDA_DIR)
+                        ida_dir=DEFAULT_IDA_DIR,
+                        actions='send')
     args = parser.parse_args()
 
     logger.info(f"collecting all IP addresses")
@@ -70,24 +76,7 @@ def _main():
         ips[region] = ips1
         all_ips.extend(ips1)
 
-    logger.info(f"generating configurations")
-
-    all_peers_config = io.StringIO()
-    peer_configs = [io.StringIO() for ip in all_ips]
-    for idx, ip in enumerate(all_ips):
-        pk = hashlib.sha1(ip.encode()).hexdigest()
-        print(f"{idx} {ip} 20000 10000 {pk} 2", file=all_peers_config)
-        with open(f'configs/{idx}.txt', 'w') as peer_config:
-            print(f"{idx} {ip} 20000 10000 {pk} 0", file=peer_configs[idx])
-            for idx2, ip2 in enumerate(all_ips):
-                if idx2 == idx:
-                    continue
-                pk2 = hashlib.sha1(ip2.encode()).hexdigest()
-                print(f"{idx2} {ip2} 20000 10000 {pk2} 1",
-                      file=peer_configs[idx])
-    all_peers_config = all_peers_config.getvalue().encode()
-    for idx, peer_config in enumerate(peer_configs):
-        peer_configs[idx] = peer_config.getvalue().encode()
+    ida_dir = sq(args.ida_dir)
 
     def ssh(ip, cmd, **kwargs):
         return subprocess.run(['ssh',
@@ -98,19 +87,93 @@ def _main():
                                f'-i{sq(args.ssh_key)}',
                                f'{args.ssh_user}@{ip}', cmd],
                               **kwargs)
-    ida_dir = sq(args.ida_dir)
 
-    logger.info(f"copying all-peer configs")
-    for idx, ip in enumerate(all_ips):
-        logger.info(f"... {ip}")
-        ssh(ip, f'cat > {ida_dir}/configs/all_peers.txt',
-            input=all_peers_config, check=True)
+    for action in args.actions.split(','):
+        action = action.strip()
+        if action == 'gen':
+            logger.info(f"generating configurations")
 
-    logger.info(f"copying per-peer configs")
-    for idx, ip in enumerate(all_ips):
-        logger.info(f"... {ip}")
-        ssh(ip, f'cat > {ida_dir}/configs/config.txt',
-            input=peer_configs[idx], check=True)
+            all_peers_config = io.StringIO()
+            peer_configs = [io.StringIO() for ip in all_ips]
+            for idx, ip in enumerate(all_ips):
+                pk = hashlib.sha1(ip.encode()).hexdigest()
+                print(f"{idx} {ip} 20000 10000 {pk} 2", file=all_peers_config)
+                with open(f'configs/{idx}.txt', 'w') as peer_config:
+                    print(f"{idx} {ip} 20000 10000 {pk} 0", file=peer_configs[idx])
+                    for idx2, ip2 in enumerate(all_ips):
+                        if idx2 == idx:
+                            continue
+                        pk2 = hashlib.sha1(ip2.encode()).hexdigest()
+                        print(f"{idx2} {ip2} 20000 10000 {pk2} 1",
+                              file=peer_configs[idx])
+            all_peers_config = all_peers_config.getvalue().encode()
+            for idx, peer_config in enumerate(peer_configs):
+                peer_configs[idx] = peer_config.getvalue().encode()
+
+            logger.info(f"copying all-peer configs")
+            for idx, ip in enumerate(all_ips):
+                logger.info(f"... {ip}")
+                ssh(ip, f'cat > {ida_dir}/configs/all_peers.txt',
+                    input=all_peers_config, check=True)
+
+            logger.info(f"copying per-peer configs")
+            for idx, ip in enumerate(all_ips):
+                logger.info(f"... {ip}")
+                ssh(ip, f'cat > {ida_dir}/configs/config.txt',
+                    input=peer_configs[idx], check=True)
+
+        elif action == 'start':
+            logger.info(f"starting server")
+            for idx, ip in enumerate(all_ips):
+                logger.info(f"... {ip}")
+                if idx == 0:
+                    continue
+                ssh(ip,
+                    f'cd {ida_dir} && ls -l ida && {{ ./ida '
+                    '-nbr_config configs/config.txt '
+                    '-all_config configs/all_peers.txt '
+                    '> ida.out 2>&1 & ls -l ida.out || :; }',
+                    check=True)
+
+        elif action == 'stop':
+            logger.info(f"stopping server")
+            for idx, ip in enumerate(all_ips):
+                logger.info(f"... {ip}")
+                ssh(ip, f'killall ida')
+
+        elif action == 'send':
+            if args.file is None:
+                parser.error("--file not specified")
+            logger.info(f"sending one file: {args.file}")
+            with open(args.file, 'rb') as f:
+                contents = f.read()
+            hex_hash = hashlib.sha1(contents).hexdigest()
+            logger.info(f"{len(contents)} byte(s), sha1 {hex_hash}")
+            logger.info(f"copying over")
+            ssh(all_ips[0], f'cat > {ida_dir}/{hex_hash}.dat',
+                    input=contents, check=True)
+            logger.info(f"invoking ida")
+            ssh(all_ips[0],
+                f'cd {ida_dir} && ./ida '
+                f'-nbr_config configs/config.txt '
+                f'-all_config configs/all_peers.txt '
+                f'-broadcast -msg_file {hex_hash}.dat '
+                f'-t0 5 '
+                f'-t1 50 '
+                f'-base 1.01 '
+                f'2>&1 | tee ida.out',
+                check=True)
+
+        elif action == 'update':
+            logger.info(f"downloading new binary from s3")
+            url = 'https://s3.us-east-2.amazonaws.com/harmony-ida-binary/ida'
+            for ip in all_ips:
+                logger.info(f"... {ip}")
+                ssh(ip,
+                    f'cd {ida_dir} && '
+                    f'rm -f ida && '
+                    f'curl -LsS -o ida {url} && '
+                    f'chmod a+x ida')
 
 
 if __name__ == '__main__':
